0e03f6ecc9d986e76136e1f4b77bd4fe094edd8b
smresponsibilities <sm.responsibilities@gmail.com>
2024-12-02 12:13:16 +0530

    style(ui): Change frontend style and format

--- mernproject/src/components/card.jsx
+++ mernproject/src/components/card.jsx
@@ -1,13 +1,12 @@
-
-
-export default function Card({children}) {
-
-    return (
-      <div className="bg-blue-200  max-h-[600px] min-w-[450px] p-5">
-        {children}
-
+export default function Card({ children }) {
+  return (
+    <div
+      className="bg-zinc-500 bg-clip-padding backdrop-filter backdrop-blur bg-opacity-30 backdrop-saturate-100 backdrop-contrast-100 
+      shadow-[8px_4px_252px_19px_rgba(96,_18,_159,_0.55)]
+      max-h-[600px] min-w-[450px] min-h-[450px] p-5"> 
       
-      </div>
-    )
-  }
-  
+      {children}
+    
+    </div>
+  );
+}
--- mernproject/src/pages/landing.jsx
+++ mernproject/src/pages/landing.jsx
@@ -1,10 +1,18 @@
-
+import Card from "../components/card";
+import "./landingpage.css";
+// import React from "react";
 
 
 export default function LandingPage() {
 
     return (
-      <div className="bg-black h-screen">
+      <div className="gradient h-screen flex items-center justify-center">
+
+        <Card/>
+
+
+
+        
 
       
       </div>
