bbef626915b4ccac9ae237319fc63e273183fad7
smresponsibilities <sm.responsibilities@gmail.com>
2024-12-02 11:06:16 +0530

    feat(middleware) : Implement Auth middleware

---
+++ Mernbackend/middlewares/auth.middleware.js
@@ -0,0 +1,21 @@
+import jwt from 'jsonwebtoken';
+
+
+
+export function AuthMiddleware(req,res,next){
+    const token = req.header('authorization').split(' ')[1];
+
+    if(!token){
+      return res.status(401).json({message: "No token, authorization denied."});
+    }
+    try{
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = decoded;
+      next();
+    }catch(e){
+      res.status(400).json({message: "token is not valid."});
+    }
+
+  
+  }
+  
--- Mernbackend/routes/user.routes.js
+++ Mernbackend/routes/user.routes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { signin, signup } from "../controllers/user.controller.js";
+ import { AuthMiddleware } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.post("/signup", signup);
 router.post("/signin", signin);
+
+
 
 export default router;
